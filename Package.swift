06edc58bd3ecb3ef6dc7fef025401fb7426424d6
Mohamed Amine <133100433+medAmineSwi@users.noreply.github.com>
2025-04-26 18:41:01 +0200

    exclude demo

--- Package.swift
+++ Package.swift
@@ -15,7 +15,11 @@ let package = Package(
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "LocalizationExtractor"),
+            name: "LocalizationExtractor",
+            exclude: [
+                "Demo"
+            ]),
+
         .testTarget(
             name: "LocalizationExtractorTests",
             dependencies: ["LocalizationExtractor"]
