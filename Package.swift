677b7d1a30c11b2ecfbd925bddc4afa90aff532c
swimoden <souiden.medamine@gmail.com>
2025-04-29 18:11:42 +0200

    fix package

--- Package.swift
+++ Package.swift
@@ -24,7 +24,7 @@ let package = Package(
         ),
         .testTarget(
             name: "LocalizationExtractorTests",
-            dependencies: ["LocalizationExtractor"],
+            dependencies: ["LocalizationExtractor"]
         ),
     ]
 )
